5c426799d95f6f075cf128f78c800b27b01e87fb
Nick <nbeirne@users.noreply.github.com>
2025-12-18 12:43:35 -0500

    fix migration

--- surveys/migrations/0004_remove_surveysubmission_cohort_and_more.py
+++ surveys/migrations/0004_remove_surveysubmission_cohort_and_more.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('surveys', '0003_add_survey_data'),
+        ('surveys', '0002_survey_alter_exitsurvey_unique_together_and_more'),
     ]
 
     operations = [
