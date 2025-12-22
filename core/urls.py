a40d7e01a2b9963a08142e8ea53037dc0fd43d09
Nick <nbeirne@users.noreply.github.com>
2025-12-22 14:33:05 -0500

    add feedback link

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import privacy_policy, protocol_view, resources_view, landing
+from .views import privacy_policy, protocol_view, resources_view, landing, feedback_view
 
 app_name = 'core'
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path('resources/', resources_view, name='resources'),
     path('', landing, name='index_landing'),
     path('landing/', landing, name='landing'),
+    path('feedback/', feedback_view, name='feedback'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpRequest, HttpResponse
 
 # This is a temporary landing page which exists until the rest of the application is ready.
@@ -20,3 +20,8 @@ def protocol_view(request: HttpRequest) -> HttpResponse:
 def resources_view(request: HttpRequest) -> HttpResponse:
     """Resources page."""
     return render(request, 'core/resources.html')
+
+
+def feedback_view(request: HttpRequest) -> HttpResponse:
+    """Feedback page."""
+    return redirect("https://docs.google.com/forms/d/e/1FAIpQLSdb0eSiUg6NS4Etj06TYLdbF0LAX-e1wrvHQHsM67VcABbTjQ/viewform?usp=dialog")
