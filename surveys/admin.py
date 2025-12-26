491f0ad3b9375da0feb5d54931e000ae198ac20e
Nick <nbeirne@users.noreply.github.com>
2025-12-26 13:03:01 -0500

    fix admin panel

--- surveys/admin.py
+++ surveys/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Survey, Question, SurveySubmission, Answer
 from cohorts.models import UserSurveyResponse
+from cohorts.tasks import find_due_date
 
 class QuestionInline(admin.TabularInline):
     """
@@ -31,7 +32,7 @@ class UserSurveyResponseInline(admin.StackedInline):
     model = UserSurveyResponse
     extra = 1
     max_num = 1
-    fields = ('user', 'cohort', 'due_date')
+    fields = ('user', 'cohort', 'scheduler', 'task_instance_id')
 
 class AnswerInline(admin.TabularInline):
     """
@@ -45,7 +46,7 @@ class AnswerInline(admin.TabularInline):
     def get_queryset(self, request):
         """Ensure we only show answers that are linked to a question."""
         qs = super().get_queryset(request)
-        return qs.filter(question__isnull=False)
+        return qs.filter(question__isnull=False, submission__isnull=False)
 
     def has_add_permission(self, request, obj=None):
         return False
@@ -53,32 +54,31 @@ class AnswerInline(admin.TabularInline):
 
 @admin.register(SurveySubmission)
 class SurveySubmissionAdmin(admin.ModelAdmin):
-    list_display = ('survey', 'get_user', 'get_cohort', 'get_due_date', 'completed_at')
+    list_display = ('survey', 'get_user',  'get_due_date', 'get_cohort', 'completed_at')
     list_filter = ('survey', 'completed_at')
-    search_fields = ('survey__name', 'user_responses__user__email', 'user_responses__cohort__name')
+    search_fields = ('survey__name', 'user_response__user__email', 'user_response__cohort__name')
     date_hierarchy = 'completed_at'
     inlines = [UserSurveyResponseInline, AnswerInline]
     readonly_fields = ('completed_at',)
 
     def get_user(self, obj):
-        return obj.user_responses.first().user if obj.user_responses.exists() else 'N/A'
+        return obj.user_response.user if hasattr(obj, 'user_response') else 'N/A'
     get_user.short_description = 'User'
-    get_user.admin_order_field = 'user_responses__user'
+    get_user.admin_order_field = 'user_response__user'
 
     def get_cohort(self, obj):
-        return obj.user_responses.first().cohort if obj.user_responses.exists() else 'N/A'
+        return obj.user_response.cohort if hasattr(obj, 'user_response') else 'N/A'
     get_cohort.short_description = 'Cohort'
-    get_cohort.admin_order_field = 'user_responses__cohort'
+    get_cohort.admin_order_field = 'user_response__cohort'
 
     def get_due_date(self, obj):
-        return obj.user_responses.first().due_date if obj.user_responses.exists() else 'N/A'
+        return find_due_date(obj.user_response.scheduler, obj.user_response.task_instance_id) if hasattr(obj, 'user_response') else 'N/A'
     get_due_date.short_description = 'Due Date'
-    get_due_date.admin_order_field = 'user_responses__due_date'
 
     def get_form(self, request, obj=None, **kwargs):
         """Dynamically set readonly_fields for answers."""
         form = super().get_form(request, obj, **kwargs)
         if obj: # If the object is saved and has a survey
             questions = obj.survey.questions.all()
-            Answer.objects.bulk_create([Answer(submission=obj, question=q) for q in questions if not obj.answers.filter(question=q).exists()], ignore_conflicts=True)
+            Answer.objects.bulk_create([Answer(submission=obj, question=q) for q in questions if not obj.answers.filter(question=q).exists() and not q.question_type == Question.QuestionType.INFO], ignore_conflicts=True)
         return form
