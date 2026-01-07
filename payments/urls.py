c5606edf65a324255dacd93b33133345186b7a00
Nick <nbeirne@users.noreply.github.com>
2026-01-07 13:34:42 -0500

    fix urls

--- payments/urls.py
+++ payments/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('create-checkout/<int:cohort_id>/', create_checkout_session, name='create_checkout'),
     path('success/', payment_success, name='success'),
     path('cancel/', payment_cancel, name='cancel'),
-    path('webhook/', stripe_webhook, name='webhook'),
+    path('webhooks/stripe/', stripe_webhook, name='webhook'),
 ]
 
