30943985c036c61788da00cb7af94dbc60582040
Nick <nbeirne@users.noreply.github.com>
2026-01-08 22:25:23 -0500

    attempt url fix

--- payments/urls.py
+++ payments/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('create-checkout/<int:cohort_id>/', create_checkout_session, name='create_checkout'),
     path('success/', payment_success, name='success'),
     path('cancel/', payment_cancel, name='cancel'),
-    path('stripe/', include(djstripe.urls)),
+    path('stripe/', include(djstripe.urls, namespace='djstripe')),
 ]
